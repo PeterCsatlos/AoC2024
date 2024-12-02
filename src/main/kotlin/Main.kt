a93176113882c3d92a8ed793b0c068fd30b7dd0b
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-02 16:55:07 +0100

    Day2 Task2 Quick implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,22 +9,13 @@ private fun safeCount(lines: List<String>) {
     var safeCount = 0
 
     lines.forEach { line ->
-        var isSafe = true
 
         val values = line.split(" ")
-        var previous = values[0].toInt()
-        val direction = values[1].toInt() < values[0].toInt()
-        values.subList(1, values.size).forEach { value ->
-            val intValue = value.toInt()
-            if (abs(previous - intValue) > 3 || abs(previous - intValue) < 1) {
-                isSafe = false
-            }
-            if (direction && intValue > previous) {
-                isSafe = false
-            } else if (!direction && intValue < previous) {
-                isSafe = false
-            }
-            previous = intValue
+        var deletedIndex = 0
+        var isSafe = isSafeCheck(values)
+        while (!isSafe && deletedIndex < values.size) {
+            isSafe = isSafeCheck(values.filterIndexed { index, _ -> index != deletedIndex })
+            deletedIndex++
         }
 
         if (isSafe) {
@@ -32,4 +23,23 @@ private fun safeCount(lines: List<String>) {
         }
     }
     println(safeCount)
+}
+
+private fun isSafeCheck(values: List<String>): Boolean {
+    var isSafe = true
+    var previous = values[0].toInt()
+    val direction = values[1].toInt() < values[0].toInt()
+    values.subList(1, values.size).forEach { value ->
+        val intValue = value.toInt()
+        if (abs(previous - intValue) > 3 || abs(previous - intValue) < 1) {
+            isSafe = false
+        }
+        if (direction && intValue > previous) {
+            isSafe = false
+        } else if (!direction && intValue < previous) {
+            isSafe = false
+        }
+        previous = intValue
+    }
+    return isSafe
 }
