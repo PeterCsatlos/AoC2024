8ea6a4922d113049a35b80c1d99166bcc66a6031
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-02 13:01:21 +0100

    Day1 Task2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,37 @@
 import kotlin.math.abs
 
 fun main(args: Array<String>) {
-    val lines = object {}.javaClass.getResourceAsStream(args[0])?.bufferedReader()?.readLines()
-    val lefts = lines?.map { it.split("  ")[0] } ?: emptyList()
-    val rights = lines?.map { it.split("  ")[1] } ?: emptyList()
+    val lines = readInput(args)
+    distanceBetweenLists(lines ?: emptyList())
+    similarityCheck(lines ?: emptyList())
+}
+
+private fun readInput(args: Array<String>): List<String>? {
+    return object {}.javaClass.getResourceAsStream(args[0])?.bufferedReader()?.readLines()
+}
+
+fun distanceBetweenLists(lines: List<String>) {
+    val lefts = lines.map { it.split("  ")[0] }
+    val rights = lines.map { it.split("  ")[1] }
 
     val leftOrdered = lefts.sorted()
     val rigthOrdered = rights.sorted()
 
-   println(leftOrdered.zip(rigthOrdered).map { (left, right) ->
+    println("Distance: " + leftOrdered.zip(rigthOrdered).map { (left, right) ->
         abs(left.toFloat() - right.toFloat())
     }.sum())
+}
+
+
+fun similarityCheck(lines: List<String>) {
+    val lefts = lines.map { it.split("  ")[0] }
+    val rights = lines.map { it.split("  ")[1] }
+
+    val mapOfAppearance: Map<Double, Int> = rights.groupingBy { it.toDouble() }.eachCount()
+
+    println(
+        lefts.sumOf {
+            ((mapOfAppearance[it.toDouble()]?.toDouble() ?: 0.0) * it.toDouble())
+        }
+    )
 }
