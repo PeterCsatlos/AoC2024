868b346e8a46e6d463e19eb9625f11a886903e1f
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-02 15:23:02 +0100

    Day2 Task1 Quick implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.abs
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
+    safeCount(lines)
+}
+
+private fun safeCount(lines: List<String>) {
+    var safeCount = 0
+
+    lines.forEach { line ->
+        var isSafe = true
+
+        val values = line.split(" ")
+        var previous = values[0].toInt()
+        val direction = values[1].toInt() < values[0].toInt()
+        values.subList(1, values.size).forEach { value ->
+            val intValue = value.toInt()
+            if (abs(previous - intValue) > 3 || abs(previous - intValue) < 1) {
+                isSafe = false
+            }
+            if (direction && intValue > previous) {
+                isSafe = false
+            } else if (!direction && intValue < previous) {
+                isSafe = false
+            }
+            previous = intValue
+        }
+
+        if (isSafe) {
+            safeCount++
+        }
+    }
+    println(safeCount)
 }
