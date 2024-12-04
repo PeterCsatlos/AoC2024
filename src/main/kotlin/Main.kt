1e82c086653b94dc689dd0272552411bd35dad40
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-04 10:43:54 +0100

    Day4 Task2 Quick implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,51 @@ fun main(args: Array<String>) {
 
     val matrix = Array(lines.size) { rowindex -> lines[rowindex].toCharArray() }
 
+    var xMasCount = 0
+
+    for ((i,row) in matrix.withIndex()) {
+        for ((j, column) in row.withIndex()) {
+            if (column == 'A') {
+                var iLeap = i - 1
+                var jLeap = j - 1
+                if (indexesCheck(iLeap, matrix, jLeap, row) && (matrix[iLeap][jLeap] == 'M' || matrix[iLeap][jLeap] == 'S')) {
+
+                    if(indexesCheck(i+1, matrix, j+1, row) &&
+                        (
+                                (matrix[iLeap][jLeap] == 'M' && matrix[i+1][j+1] == 'S') ||
+                                (matrix[iLeap][jLeap] == 'S' && matrix[i+1][j+1] == 'M'))
+                        ) {
+                        iLeap = i - 1
+                        jLeap = j + 1
+                        if (indexesCheck(
+                                iLeap,
+                                matrix,
+                                jLeap,
+                                row
+                            ) && (matrix[iLeap][jLeap] == 'M' || matrix[iLeap][jLeap] == 'S')
+                        ) {
+
+                            if (indexesCheck(i+1, matrix, j-1, row) &&
+                                (
+                                        (matrix[iLeap][jLeap] == 'M' && matrix[i + 1][j - 1] == 'S') ||
+                                                (matrix[iLeap][jLeap] == 'S' && matrix[i + 1][j - 1] == 'M'))
+                            ) {
+                                xMasCount++
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println(xMasCount)
+
+// countXmas(matrix)
+
+}
+
+private fun countXmas(matrix: Array<CharArray>) {
     var xmasCount = 0
 
     for ((i,row) in matrix.withIndex()) {
