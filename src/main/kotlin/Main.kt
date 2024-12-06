481b2a8324f217b6282cd67ca67d533605e7bbc6
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-06 09:01:21 +0100

    Day6 Task1 Quick implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,65 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var index: Pair<Int, Int> = Pair(1, 1)
+    val matrix = Array(lines.size) { rowindex -> lines[rowindex].toCharArray() }
+
+    matrix.forEachIndexed { lineIndex, line ->
+        if (line.indexOf('^') != -1) {
+            index = Pair(lineIndex, line.indexOf('^'))
+        }
+    }
+
+    var movement = matrix[index.first][index.second]
+    while (index.first >= 0 && index.first <= matrix.size-1 && index.second >= 0 && index.second <= matrix[0].size-1) {
+        matrix[index.first][index.second] = 'X'
+        if (movement == '^') {
+            if (index.first == 0) {
+                index = Pair(-1, -1)
+            } else {
+                if (matrix[index.first - 1][index.second] == '#') {
+                    movement = '>'
+                } else {
+                    index = Pair(index.first - 1, index.second)
+                }
+            }
+        } else if (movement == '>') {
+            if (index.second == matrix[0].size-1) {
+                index = Pair(-1, matrix[0].size + 1)
+            } else {
+                if (matrix[index.first][index.second + 1] == '#') {
+                    movement = 'v'
+                } else {
+                    index = Pair(index.first, index.second + 1)
+                }
+            }
+        } else if (movement == 'v') {
+            if (index.first == matrix.size-1) {
+                index = Pair(matrix.size + 1, matrix[0].size + 1)
+            } else {
+                if (matrix[index.first + 1][index.second] == '#') {
+                    movement = '<'
+                } else {
+                    index = Pair(index.first + 1, index.second)
+                }
+            }
+        } else if (movement == '<') {
+            if (index.second == 0) {
+                index = Pair(-1, -1)
+            } else {
+            if (matrix[index.first][index.second - 1] == '#') {
+                movement = '^'
+            } else {
+                index = Pair(index.first, index.second - 1)
+            }}
+        }
+    }
+
+    var count = 0
+
+    matrix.forEach { line ->
+        count += line.count { char -> char == 'X' }
+    }
+
+    println(count)
 }
