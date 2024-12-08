b267dc5dda0e182705ee12d0416263591179f079
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-08 12:11:31 +0100

    Day7 Task1 Implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,53 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var validResult = 0.toDouble()
+    lines.forEach {line ->
+        val result = line.split(':')[0].toDouble()
+        val equiasion = line.split(':')[1]
+
+        val operands = equiasion.trim().split(' ')
+        val bitNumber = operands.size-1
+        val binaryNumber = MutableList(bitNumber) { 0 }
+        val maxBinaryNumber = MutableList(bitNumber) { 1 }
+        var valid = false
+        println("Initial Binary Number: ${binaryNumber.joinToString("")}")
+        println("Operands: $operands")
+        println("Result: $result")
+        var max = false
+        while (!max && !valid) {
+            println("Increment ${binaryNumber.joinToString("")}")
+            var currentSum = operands[0].toDouble()
+
+            for (i in 0..<bitNumber) {
+                if (binaryNumber[i] == 0) {
+                    currentSum += operands[i + 1].toDouble()
+                } else {
+                    currentSum *= operands[i + 1].toDouble()
+                }
+            }
+            if (result == currentSum) {
+                valid = true
+                validResult += currentSum
+                println("VALID NUMBER: $validResult")
+            }
+            if (binaryNumber == maxBinaryNumber) {
+                max = true
+                println("MAX")
+            }
+            incrementBinary(binaryNumber)
+        }
+
+        println(validResult)
+    }
+}
+
+fun incrementBinary(binaryNumber: MutableList<Int>) {
+    var carry = 1
+    for (i in binaryNumber.indices.reversed()) {
+        val sum = binaryNumber[i] + carry
+        binaryNumber[i] = sum % 2
+        carry = sum / 2
+        if (carry == 0) break
+    }
 }
