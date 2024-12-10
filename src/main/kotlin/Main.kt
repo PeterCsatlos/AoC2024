bd6e18a546df62b6ef15c801cd2709d791d8d4f5
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-11 00:05:26 +0100

    Day8 Task2 Implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
-fun main(args: Array<String>) {
+fun main() {
     val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
 
     val matrix = Array(lines.size) { rowindex -> lines[rowindex].toCharArray() }
-    val nodeMatrix = createNodeMatrix(matrix)
 
     val zeros = mutableListOf<Pair<Int,Int>>()
 
@@ -14,6 +13,9 @@ fun main(args: Array<String>) {
             }
         }
     }
+
+    antennas.entries.removeIf { (_, positions) -> positions.size == 1 }
+
 
     matrix.forEachIndexed { lineIndex, line ->
         line.forEachIndexed { charIndex, char ->
@@ -32,22 +34,19 @@ fun main(args: Array<String>) {
             while (j < positions.size) {
                 val first = positions[i]
                 val second = positions[j]
+                crossMatrix[first.first][first.second] = "#"
+                crossMatrix[second.first][second.second] = "#"
 
                 val xleap1 = first.first - second.first // -1
                 val yleap1 = first.second - second.second // 3
-                val antinode1 = Pair(first.first + xleap1, first.second + yleap1)
-
-                if (antinode1.first >=0 && antinode1.first < matrix.size && antinode1.second >= 0 && antinode1.second < matrix[0].size) {
-                    crossMatrix[antinode1.first][antinode1.second] = "#"
-                }
+                var step = first
+                addEveryValidAntinode(step, xleap1, yleap1, matrix, crossMatrix)
 
                 val xleap2 = second.first - first.first // 1
                 val yleap2 = second.second - first.second // -3
-                val antinode2 = Pair(second.first+xleap2, second.second + yleap2 )
+                step = second
+                addEveryValidAntinode(step, xleap2, yleap2, matrix, crossMatrix)
 
-                if (antinode2.first >=0 && antinode2.first < matrix.size && antinode2.second >= 0 && antinode2.second < matrix[0].size) {
-                    crossMatrix[antinode2.first][antinode2.second] = "#"
-                }
                 j++
             }
             i++
@@ -66,9 +65,23 @@ fun main(args: Array<String>) {
     println(result)
 }
 
-fun createNodeMatrix(matrix: Array<CharArray>): Array<MutableList<String>> {
-    return Array(matrix.size) { rowIndex ->
-        matrix[rowIndex].copyOf().map { it.toString() }.toMutableList()
+private fun addEveryValidAntinode(
+    step: Pair<Int, Int>,
+    xleap1: Int,
+    yleap1: Int,
+    matrix: Array<CharArray>,
+    crossMatrix: Array<MutableList<String>>
+) {
+    var end = false
+    var step1 = step
+    while (!end) {
+        val antinode = Pair(step1.first + xleap1, step1.second + yleap1)
+        step1 = antinode
+        if (antinode.first >= 0 && antinode.first < matrix.size && antinode.second >= 0 && antinode.second < matrix[0].size) {
+            crossMatrix[antinode.first][antinode.second] = "#"
+        } else {
+            end = true
+        }
     }
 }
 
