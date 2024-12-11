508e393e590770a4448b28c39b56d17e0fd72525
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-11 01:19:47 +0100

    Day9 Task1 Implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,48 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val blocks = mutableListOf<String>()
+    var isFile = true
+    var index = 0
+    lines[0].forEach {
+        val current = it.toString().toInt()
+        if (isFile) {
+            for (i in 0..<current) {
+                blocks.add(index.toString())
+            }
+            index++
+        } else {
+            for (i in 0 ..<current) {
+                blocks.add(".")
+            }
+        }
+        isFile = !isFile
+    }
+    val emptyIndexes = mutableListOf<Int>()
+    blocks.forEachIndexed { charIndex, char ->
+        if(char== ".") {
+            emptyIndexes.add(charIndex)
+        }
+    }
+
+    var end = blocks.size-1
+    while (emptyIndexes.size > 0) {
+        if (blocks.elementAt(end) == ".") {
+            emptyIndexes.removeLast()
+            end--
+        } else {
+            blocks[emptyIndexes[0]] = blocks.elementAt(end)
+            blocks[end] = "."
+            end--
+            emptyIndexes.removeAt(0)
+        }
+    }
+
+    var result = 0.toLong()
+    index = 0
+    while (index<= end) {
+        result += blocks[index].toInt() * index
+        index++
+    }
+    println(result)
 }
