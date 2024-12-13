c5fd4ac3abe95f6adea128ebd1d202539736b133
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-13 13:40:27 +0100

    Day11 Task1 Implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.LinkedList
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val lines = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
+
+    val line = LinkedList<Long>()
+    line.addAll(lines[0].split(' ').map { it.toLong() })
+
+    for (blink in 0..<25) {
+        var index = 0
+        while (index < line.size) {
+            val current = line[index]
+            if (current == 0.toLong()) {
+                line[index] = 1
+            } else if (current.toString().length % 2 == 0) {
+                line[index] = current.toString().substring(0,(current.toString().length/2)).toLong()
+                line.add(index+1, current.toString().substring(current.toString().length/2).toLong())
+                index++
+            } else {
+                line[index] = current * 2024
+            }
+            index++
+        }
+    }
+    println(line.size)
 }
+
