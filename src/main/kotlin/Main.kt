7a376568e19dddd0d96ba3db12e8b6e43fafdf8a
petercsatlos <PeterMark.Csatlos@erstegroup.com>
2024-12-30 14:04:31 +0100

    Day14 Task1 Implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,66 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val map = object {}.javaClass.getResourceAsStream("input.txt")?.bufferedReader()?.readLines() ?: emptyList()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val robots = mutableListOf<Robot>()
+
+    map.forEach {line ->
+        val posAndSpeed = line.split(" ")
+        val pos = posAndSpeed[0].split("=")[1].split(",").let { Pair(it[0].toInt(), it[1].toInt()) }
+        val speed = posAndSpeed[1].split("=")[1].split(",").let { Pair(it[0].toInt(), it[1].toInt()) }
+
+        robots.add(Robot(x = pos.first, y = pos.second, speed = speed))
+    }
+
+    robots.forEach{
+        for (i in 0..<100) {
+            it.move()
+            println(i)
+        }
+    }
+
+    val xHalf = X_MAX/2
+    val yHalf = Y_MAX/2
+
+    var first = 0L
+    var second = 0L
+    var third = 0L
+    var fourth = 0L
+
+    robots.forEach {
+        if (it.x < xHalf && it.y < yHalf) {
+            first++
+        } else if (it.x > xHalf && it.y > yHalf) {
+            fourth ++
+        } else if (it.x < xHalf && it.y > yHalf) {
+            third ++
+        } else if (it.x > xHalf && it.y < yHalf) {
+            second++
+        }
+    }
+
+    println(first*second*third*fourth)
+}
+
+private const val X_MAX = 101
+private const val Y_MAX = 103
+
+class Robot(
+    var x: Int,
+    var y: Int,
+    var speed: Pair<Int, Int>
+) {
+    fun move() {
+        x = adjust(x+speed.first, X_MAX)
+
+        y = adjust(y + speed.second, Y_MAX)
+    }
+
+    private fun adjust(coordinate: Int, max: Int): Int {
+        if (coordinate >= max) {
+            return coordinate - max
+        } else if (coordinate < 0) {
+            return max + coordinate
+        }
+        return coordinate
+    }
 }
